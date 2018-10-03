7369bec51235a0ba1bf93a6c6073c04453c42354
syalih <syalih@users.noreply.github.com>
2018-10-03 07:09:00 +0530

    Create news_log.py

---
+++ news_log.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+def get_results(query):
+	db = psycopg2.connect("dbname=news")
+	c = conn.cursor()
+	c.execute(query)
+	rows = cursor.fetchall()
+	return rows
+	db.close()
+
+
+
+def popular_articles():
+
+
+
+
+def popular_article_authors():
+
+
+
+def error_day_percen():
+
+	
